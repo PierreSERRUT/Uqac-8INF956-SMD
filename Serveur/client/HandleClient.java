9376c9a8f7fd332d446a696c81214028a3d856b3
hugovdb <hvandenbunder@live.fr>
2017-06-18 11:56:41 -0400

    HandleClient w/ socket

--- Serveur/client/HandleClient.java
+++ Serveur/client/HandleClient.java
@@ -1,10 +1,22 @@
 package Serveur.client;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 
 public class HandleClient implements Runnable {
 
-    private Socket clientSocket;	
+    private Socket clientSocket;
+    private InputStream clientInput;
+    private OutputStream clientOutput;
+    private ObjectInputStream clientObject;
+    private Object objectInput;
+    private DataInputStream din;
+    private DataOutputStream dout;
 	
 	public HandleClient(Socket clientSock){
 		this.clientSocket = clientSock;
@@ -13,6 +25,24 @@ public class HandleClient implements Runnable {
 	 @Override
 	    public void run() {
 		 System.out.println("New client");
+         
+		 try{
+		 clientInput = clientSocket.getInputStream();
+		 clientOutput = clientSocket.getOutputStream();
+         clientObject = new ObjectInputStream(clientInput);
+         //objectInput = clientObject.readObject();
+         din = new DataInputStream(clientInput);
+         dout = new DataOutputStream(clientOutput);
+         
+         String mess = din.readUTF();
+         System.out.println(mess);
+         
+         dout.writeUTF("yolo");
+         
+		 }
+		 catch (IOException e) {
+			 
+		 }
 		 
 	 }
 	
