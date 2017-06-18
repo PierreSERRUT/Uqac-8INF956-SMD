82c6ad4082f7bf4384e9e3aa027eab88310c8810
Pierre SERRUT <pserrut@gmail.com>
2017-06-18 13:32:58 -0400

    ListCalcul

--- calcul/CaculList.java
+++
@@ -1,9 +0,0 @@
-package calc;
-
-import java.util.ArrayList;
-
-public class CaculList {
-	private Calcul [] listCurrentCalc;
-	private ArrayList<Calcul> maListe = new ArrayList<Calcul>();
-
-}
---
+++ calcul/CalculList.java
@@ -0,0 +1,14 @@
+package calc;
+
+import java.util.ArrayList;
+
+
+public class CalculList {
+	
+	private ArrayList<Calcul> listCalc;
+
+
+public CalculList() {
+	listCalc = new ArrayList<Calcul>();
+	}
+}
