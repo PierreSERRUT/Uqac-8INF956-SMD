20a2f29865afbb614df83e2e97a435c9ae6aac89
ArokThar <jeremie93@laposte.net>
2017-06-19 19:17:40 +0200

    Maj ClientList
    ClientList est un singleton

--- Serveur/src/client/Client.java
+++ Serveur/src/client/Client.java
@@ -1,7 +1,26 @@
 package client;
 
+import java.util.Date;
+
 public class Client {
 
 	public HandleClient handleClient;
-	public int userid;
+	private int userid;
+	private Date LastCo;
+	
+	public Client(int id, HandleClient hc){
+		this.userid = id;
+		this.handleClient = hc;
+	}
+	
+	public Date getLastCo() {
+		return LastCo;
+	}
+	public void setLastCo(Date lastCo) {
+		LastCo = lastCo;
+	}
+	public int getUserid() {
+		return userid;
+	}
+	
 }
